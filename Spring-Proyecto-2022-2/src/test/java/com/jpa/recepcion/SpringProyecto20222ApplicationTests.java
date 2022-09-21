ad5f01ba724eabac3ea5b6cbe0b376577c04170d
markedws21 <espinozarojasmark@gmail.com>
2022-09-21 14:54:11 -0500

    PROYECTO RECEPCION

---
+++ Spring-Proyecto-2022-2/src/test/java/com/jpa/recepcion/SpringProyecto20222ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.recepcion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProyecto20222ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

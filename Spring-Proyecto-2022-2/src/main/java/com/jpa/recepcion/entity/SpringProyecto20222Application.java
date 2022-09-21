44cf2191de8307065a22b7ca8df0996cffa2f79c
markedws21 <espinozarojasmark@gmail.com>
2022-09-21 14:59:20 -0500

    CARPETAS PARA EL PROYECTO

---
+++ Spring-Proyecto-2022-2/src/main/java/com/jpa/recepcion/entity/SpringProyecto20222Application.java
@@ -0,0 +1,13 @@
+package com.jpa.recepcion.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProyecto20222Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProyecto20222Application.class, args);
+	}
+
+}
